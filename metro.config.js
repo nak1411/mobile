2b0e202e5f2bc277904ca827b6dd7e88bc53f63d
Justin Schlump <nak1411@gmail.com>
2025-08-21 22:43:07 -0700

    build issues

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Disable the problematic screen capture observer
+config.resolver.platforms = ["native", "android", "ios", "web"];
+
 module.exports = config;
